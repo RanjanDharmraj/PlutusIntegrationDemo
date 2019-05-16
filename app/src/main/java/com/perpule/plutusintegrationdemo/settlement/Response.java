1ca8d6b274c0cef2982d7f42f2abf979a7f17910
Lalit Kumar <lk.iitkgp@gmail.com>
2019-05-16 19:52:49 +0530

    Lalit

--- app/src/main/java/com/perpule/plutusintegrationdemo/settlement/Response.java
+++ app/src/main/java/com/perpule/plutusintegrationdemo/settlement/Response.java
@@ -1,4 +1,30 @@
 package com.perpule.plutusintegrationdemo.settlement;
 
-public class Response {
+import android.support.v7.app.AppCompatActivity;
+
+
+public class Response extends  AppCompatActivity  {
+    private String BactchName;
+    private String AcquierCode;
+    private String TID;
+    private String MID;
+    private long CreditCount;
+    private long CreditAmount;
+    private long DebitCount;
+    private long DebitAmount;
+    Object[] arr = new Object[6];
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        arr[0] = new String("First Pair");
+        arr[1] = new Integer(1);
+        arr[2] = new String("Second Pair");
+        arr[3] = new Integer(2);
+        arr[4] = new String("Third Pair");
+        arr[5] = new Integer(3);
+
+
+    }
 }
